f9f4112af3ae8c0e13d0caa47898b51237da18b4
rishavratzee <rishavforu0123@gmail.com>
2024-04-04 00:46:11 +0530

    CRUD operation in main.py

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 #App Object
 app = FastAPI()
+
+
+from BACKEND.model import Todo
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    delete_todo
+)
 
 origins = ['https://localhost:300']
 
@@ -20,20 +30,36 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo{title}", response_model=Todo)
+async def get_todo_by_id(title):
+    response = await get_todo_by_id(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no Todo item with this title
+                        {title}")
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong / Bad Request")
 
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.put("/api/todo{title}/", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no Todo item with this title
+                        {title}")
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo{title}")
+async def delete_todo(title : str):
+    response = await delete_todo(title)
+    if response:
+        return "Succesfully deleted todo item !"
+    raise HTTPException(404, f"There is no Todo item with this title
+                        {title}")
