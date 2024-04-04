d4486643f872c329d3496bac8df7d2a7de5ad21f
rishavratzee <rishavforu0123@gmail.com>
2024-04-04 19:10:27 +0530

    Fixed CORS issue

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -14,14 +14,14 @@ from database import (
     delete_todo
 )
 
-origins = ['https://localhost:3000']
+origins = ['http://localhost:3000/']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
